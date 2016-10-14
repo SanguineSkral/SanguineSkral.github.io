6171108d0947f75e30402cee8a5cadda531c3e51
SanguineSkral <sanguineskralx@gmail.com>
2016-10-14 12:23:21 -0700

    Add files via upload

---
+++ ifelse.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html><head><title>if else</title>
+
+<style>
+.r1 { background-color: #66FFFF;
+		color: #0000ff;
+	}
+
+.r2 { background-color: #0000ff;
+		color: #66ffff;
+	}
+</style>
+</head>
+<body>
+	<center>
+<h1>
+if else php example
+</h1>
+
+
+<h1><?php
+echo "<h1>";
+	for ($i = 0; $i < 100; $i++){
+		if($i % 2 == 0 ){
+				echo"<div class=\"r1\">";
+			}else{
+			echo<div class=\"r2\">";
+		}
+		
+		echo "* * * * * * * * $i * * * * * * * *<br />";
+		echo "/div>";
+		
+	}
+?>
+</h1>
+</center>
+</body>
+</html>
